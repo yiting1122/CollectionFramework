a9d7f659f6c6110cb9b7f3de18fb9e427a14b91f
yiting1122 <574809325@qq.com>
2014-05-13 10:17:13 +0800

    iterator

---
+++ coll/src/com/util/Iterator.java
@@ -0,0 +1,30 @@
+package com.util;
+
+
+/**
+ * 
+ * @author Administrator
+ *Iterator 是一个遍历对象，通过这个遍历对象可以对collection 对象进行遍历
+ * @param <E> E代表集合中保存的数据类型
+ */
+public interface Iterator<E>{
+	/**
+	 * 
+	 * hasNext用来判断是否有element，如果没有返回false
+	 * 
+	 * 
+	 */
+	public boolean hasNext();
+	
+	/**
+	 * next获取下一个元素，同时把索引往后面移动，从而可以继续访问后面的 类似链表操作
+	 * @return
+	 */
+	public E next();
+	
+	/**
+	 * remove用于删除一个element，但是如果在next函数之前调用remove，在下次next调用时可能产生异常
+	 * 一般都是next之后跟一个remove
+	 */
+	public void remove();
+}
