f8e70ba07646eefc2183f333c0f69586fc63f0ee
yiting1122 <574809325@qq.com>
2014-05-18 14:39:08 +0800

    AbstractCollection。java is an abstract class,some defalut implement of some functions

--- coll/src/com/util/Iterator.java
+++ coll/src/com/util/Iterator.java
@@ -25,6 +25,7 @@ public interface Iterator<E> extends java.util.Iterator<E>{
 	/**
 	 * remove用于删除一个element，但是如果在next函数之前调用remove，在下次next调用时可能产生异常
 	 * 一般都是next之后跟一个remove
+	 * 这里引入删除的好处就是方便，否则在元素删除时还要进行遍历或者保存该位置。
 	 */
 	public void remove();
 }
